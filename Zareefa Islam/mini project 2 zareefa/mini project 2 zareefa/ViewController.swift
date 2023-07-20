597c32651433abbb283260801b7964bded1376ae
zvreefa <139789537+zvreefa@users.noreply.github.com>
2023-07-20 10:48:15 -0400

    Add files via upload

---
+++ Zareefa Islam/mini project 2 zareefa/mini project 2 zareefa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mini project 2 zareefa
+//
+//  Created by SMART Scholars on 7/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var answerDisplay: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
